392fa90390bea2f48e34c59f46b0d51b52220f5e
taijusanagi <taijusanagi@gmail.com>
2023-12-04 15:57:01 +0900

    add character state in unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int humanityScore;
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
 }
